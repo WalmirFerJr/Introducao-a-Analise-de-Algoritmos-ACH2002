a2fe8aaeadaccdf40829af77606dfc149c50b01c
WalmirFerJr <walmir.jr.2004@usp.br>
2025-09-24 17:06:21 -0300

    Algoritmo de buscabinaria de maneira iterativa concluido

--- buscabinaria.c
+++ buscabinaria.c
@@ -2,20 +2,43 @@
 #include <string.h>
 #include <stdlib.h>
 
-int binary_search(int arr[]) {
+int binary_search(int arr[], int left, int right, int x) {
 
-    int start = 0;
-    int last = sizeof(arr) / sizeof(arr[0]);
+    while (left <= right) {
 
-    return start, last;
+        int mid = (left + right) / 2;
 
+        if (arr[mid] == x) {
+            return mid; 
+        }
+        else if (arr[mid] < x) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return (-1); // Caso o valor não seja encontrado na lista
 
 }
 
 int main() {
 
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {10, 20, 30, 40};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key;
 
-    printf(binary_search(arr));
+    printf("Digite um valor pra buscar no vetor:\n");
+    scanf("%d", &key);
 
+    int result = binary_search(arr, 0, size - 1, key);
+
+    if (result == (-1)) {
+        printf("Valor não encontrado.\n");
+    }
+    else {
+
+        printf("O valor %d foi encontrado na posição %d do vetor.\n", key, result);
+    }
 }
