a50491fea687b69a6ad6ca4d73e4bcf062ad3672
WalmirFerJr <walmir.jr.2004@usp.br>
2025-09-25 18:27:25 -0300

    Sync

--- buscabinaria.c
+++ buscabinaria.c
@@ -2,20 +2,31 @@
 #include <string.h>
 #include <stdlib.h>
 
-int binary_search(int arr[]) {
+int binary_search(int arr[], int left, int right, int x) {
 
-    int start = 0;
-    int last = sizeof(arr) / sizeof(arr[0]);
+    int mid = (left + right) / 2;
 
-    return start, last;
+    while(left <= right) {
 
+        if(x == mid) {
+            return mid; 
+        }
+        else if(x <= mid) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
 
+    return (-1);
 }
 
 int main() {
 
     int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf(binary_search(arr));
+    printf("%d\n",binary_search(arr, 0, (size - 1), 5));
 
 }
